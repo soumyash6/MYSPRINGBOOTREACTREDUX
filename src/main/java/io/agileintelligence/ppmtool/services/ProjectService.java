52d2ebed98e0fe80d762f88a86b31889d9f78d83
soumya <soumyash651@gmail.com>
2019-07-21 12:20:45 +0530

    added project details in database

---
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.agileintelligence.ppmtool.services;
+
+import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author SOUMYA SAHOO
+ */
+@Service
+public class ProjectService {
+
+    private final ProjectRepository projectRepository;
+
+    @Autowired
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public Project saveOrUpdateProject(Project project) {
+
+        return projectRepository.save(project);
+    }
+
+}
---
+++ src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.agileintelligence.ppmtool.web;
+
+import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author SOUMYA SAHOO
+ */
+@RestController
+@RequestMapping("/api/project")
+public class ProjectController {
+
+    private final ProjectService projectService;
+
+    @Autowired
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+        Project projectcreated = projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+}
